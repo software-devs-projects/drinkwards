2c31b6c4cffeb9cc5ae4aa3e807bae438dafda66
Anshul Mistry <amm.suthar@gmail.com>
2020-12-26 00:19:24 -0500

    rewardpoint logic added, hasura integreted with rewardpoints

--- src/BusinessHomeScreen.js
+++ src/BusinessHomeScreen.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 import { Auth } from 'aws-amplify'
 import { RNCamera } from 'react-native-camera'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome'
 import { Input } from 'react-native-elements'
+import { useMutation } from '@apollo/client'
+import { UPDATE_REWARD_POINTS } from './graphql/mutations'
+import { GET_REWARD_POINTS } from './graphql/ queries'
+
 
 const styles = StyleSheet.create({
 	container: {
@@ -14,19 +18,45 @@ const styles = StyleSheet.create({
 
 const BusinessHomeScreen = () => {
 	const [scan, setScan] = useState(true)
-	const [userId, setUserId] = useState('12345')
+	const [rpoints, setrpoints] = useState(0.0)
+	const [amount, setAmount] = useState(0.0)
+	const [userId, setUserId] = useState('')
+	const [updateRewardPoints] = useMutation(UPDATE_REWARD_POINTS)
 
 	const handleQRCodeRead = e => {
 		setEmail(e.data)
 		setScan(false)
 	}
 
+	const handleChangeAmount = (value) => {
+		setAmount(value)
+
+		setrpoints(value * 0.05)
+	}
+
 	const handleLogout = () => {
 		Auth.signOut()
 	}
-	const handleSubmit = () => {
 
+	const handleSubmit = async () => {
+
+		const user = await Auth.currentAuthenticatedUser()
+		const idToken = user.signInUserSession.idToken.jwtToken
+
+		updateRewardPoints({
+			variables: {
+				userId,
+				rewardPoints: rpoints
+			},
+			context: {
+				headers: {
+					Authorization: `Bearer ${idToken}`,
+					'x-hasura-role': 'buisness'
+				}
+			},
+		})
 	}
+
 	return (
 		<>
 			<View style={styles.container}>
@@ -50,8 +80,20 @@ const BusinessHomeScreen = () => {
 				}
 			/>
 			<Input
+				value={rpoints.toPrecision(2).toString()}
+				disabled
+				leftIcon={
+					<Icon
+						name='gift'
+						size={24}
+						color='black'
+					/>
+				}
+			/>
+			<Input
 				placeholder='Enter amount'
-				keyboardType='decimal-pad'
+				value={amount.toString()}
+				onChangeText={handleChangeAmount}
 				leftIcon={
 					<Icon
 						name='usd'
@@ -60,7 +102,7 @@ const BusinessHomeScreen = () => {
 					/>
 				}
 			/>
-			<Button onPress={handleSubmit} title={'Submit'} />
+			<Button disabled={!userId} onPress={handleSubmit} title={'Submit'} />
 			<Button onPress={handleLogout} title={'Log out'} />
 		</>
 	)
---
+++ src/graphql/mutations.js
@@ -0,0 +1,9 @@
+import { gql } from '@apollo/client'
+
+export const UPDATE_REWARD_POINTS = gql`
+    mutation updateRewardPoints($userId: uuid!, $rewardPoints: float8!) {
+        update_user_by_pk(pk_columns: {userId: $userId}, _inc: {rewardPoints: $rewardPoints}) {
+        rewardPoints
+        }
+    }
+`
